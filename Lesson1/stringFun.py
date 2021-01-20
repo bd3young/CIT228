06a325194b0dec62118e7664ba711d4c90c6bfec
Beau DeYoung <beaudeyoung@gmail.com>
2021-01-20 18:41:30 -0500

    stringFun Fixes

--- Lesson1/stringFun.py
+++ Lesson1/stringFun.py
@@ -16,3 +16,42 @@ sentence1 = "the " + adj + " " + noun + " " + verb + " " + ending + "."
 sentence2 = f"the {adj} {noun} {verb} {ending}."
 print(sentence1.upper())
 print(sentence2.lower())
+
+print("------------------------------------------")
+print("Exercise 3")
+poem = """I met a traveller from an antique land
+
+Who said: `Two vast and trunkless legs of stone
+
+Stand in the desert. Near them, on the sand,
+
+Half sunk, a shattered visage lies, whose frown,
+
+And wrinkled lip, and sneer of cold command,
+
+Tell that its sculptor well those passions read
+
+Which yet survive, stamped on these lifeless things,
+
+The hand that mocked them and the heart that fed.
+
+And on the pedestal these words appear —
+
+“My name is Ozymandias, king of kings:
+
+Look on my works, ye Mighty, and despair!”
+
+Nothing beside remains. Round the decay
+
+Of that colossal wreck, boundless and bare
+
+The lone and level sands stretch far away.’ """
+print(poem)
+
+print("------------------------------------------")
+print("Exercise 4")
+print()
+print("Colors", "\t\t", "Food", "\n")
+print("Red", "\t\t", "Pizza", "\n")
+print("Green", "\t\t", "Salad", "\n")
+print("Blue", "\t\t", "Popsicle", "\n")
