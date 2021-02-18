96c1159d78765c1213c9c7732f5d59ad3b01fe5e
Beau DeYoung <beaudeyoung@gmail.com>
2021-02-17 21:03:44 -0500

    fixed file path issue chatpter10

--- Lesson5/Chapter10/learningPython.py
+++ Lesson5/Chapter10/learningPython.py
@@ -1,17 +1,17 @@
 print('Try it Yourself 10-2')
 
-with open('learningPython.txt') as txtFile:
+with open('Lesson5\Chapter10\learningPython.txt') as txtFile:
     myTxt = txtFile.read()
 print('-------------------------Output from read()--------------------------')
 print(myTxt)
 
 print('-------------------------Output from for loop inside with...open--------------------------')
-with open('learningPython.txt') as txtFile:
+with open('Lesson5\Chapter10\learningPython.txt') as txtFile:
     for line in txtFile:
         print(line)
 
 print('-------------------------Output from readlines()--------------------------')
-with open('learningPython.txt') as txtFile:
+with open('Lesson5\Chapter10\learningPython.txt') as txtFile:
     myTxt = txtFile.readlines()
 print(f'Original List = {myTxt}')
 for line in myTxt:
@@ -20,7 +20,7 @@ for line in myTxt:
 print()
 print('Try it Yourself 10-2')
 print('-------------------------Output from readlines()--------------------------')
-with open('learningPython.txt') as txtFile:
+with open('Lesson5\Chapter10\learningPython.txt') as txtFile:
     for line in txtFile:
         print(f'Original: {line}')
         print(f'Modified: {line.replace("zz", "t")}')
