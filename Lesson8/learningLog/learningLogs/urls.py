15f82781c661b52a5e18dac209d113f88a8860b6
Beau DeYoung <beaudeyoung@gmail.com>
2021-03-30 23:44:43 -0400

    Lesson 8 Completed

--- Lesson8/learningLog/learningLogs/urls.py
+++ Lesson8/learningLog/learningLogs/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'learningLogs'
 urlpatterns = [
     path('', views.index, name = 'index'),
-    path('topics/', views.topics, name = 'topics')
+    path('topics/', views.topics, name = 'topics'),
+    path('topics/<int:topic_id>/topic/', views.topic, name = 'topic'),
 ]
