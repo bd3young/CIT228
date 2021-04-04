8075fc665fe8c62c19285be75748ca5eee351e72
Beau DeYoung <beaudeyoung@gmail.com>
2021-04-04 19:15:20 -0400

    Completed Chapter 9

--- Lesson8/learningLog/learningLog/settings.py
+++ Lesson8/learningLog/learningLog/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'learningLogs',
+    'users',
 
     'django.contrib.admin',
     'django.contrib.auth',
@@ -120,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_URL = 'users:login'
